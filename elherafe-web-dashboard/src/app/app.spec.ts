27bc0a15fedbaab884820c512ddb2b9f68bf3e41
Ehab <2100632@eng.asu.edu.eg>
2025-11-25 15:33:14 +0200

    test that the cahnge will got affected

--- elherafe-web-dashboard/src/app/app.spec.ts
+++ elherafe-web-dashboard/src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, elherafe-web-dashboard');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Helloooo, elherafe-web-dashboard');
   });
 });
