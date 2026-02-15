a2636a9f0d1261f07abddc2e5628ccff607848c6
Ehab <2100632@eng.asu.edu.eg>
2026-02-15 23:32:56 +0200

    test the new infra

--- elherafe-web-dashboard/src/environments/environment.ts
+++ elherafe-web-dashboard/src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://el7rafe-gma4hrg0e9epfsc0.uaenorth-01.azurewebsites.net'
+    // apiUrl: 'https://el7rafe-gma4hrg0e9epfsc0.uaenorth-01.azurewebsites.net'
+    apiUrl: 'https://el7rafeapp.azurewebsites.net'
+
 };
